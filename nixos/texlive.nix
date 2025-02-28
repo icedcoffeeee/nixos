0c6c1304dececa58f8bc243570cb26caee9c443d
icedcoffeeee <83535735+icedcoffeeee@users.noreply.github.com>
2025-02-28 20:29:33 +0800

    refactor texlive

--- nixos/texlive.nix
+++ nixos/texlive.nix
@@ -1,28 +1,23 @@
 { pkgs, ... }:
-rec {
-  options = {
-    texlive.enable = true;
-    texpkg = { pkgs, name, url, sha256, version }: let
-      unzip = "${pkgs.unzip}/bin/unzip";
-      rg = "${pkgs.ripgrep}/bin/rg";
-    in {
-      pkgs = [(
-        pkgs.runCommand name {
-          src = pkgs.fetchurl { inherit url sha256; };
-          passthru = {
-            pname = name;
-            inherit version;
-            tlType = "run";
-          };
-        }
-        ''
-        ${unzip} $src -d ${name}
-        mkdir -p $out/tex/latex/${name}
-        cp $(${rg} --files|${rg} sty) $out/tex/latex/${name}
-        ''
-      )];
-    };
-  };
+let
+  unzip = "${pkgs.unzip}/bin/unzip";
+  rg = "${pkgs.ripgrep}/bin/rg";
+  texpkg = { name, url, sha256, version }: [(
+    pkgs.runCommand name {
+      src = pkgs.fetchurl { inherit url sha256; };
+      passthru = {
+        inherit version;
+        pname = name;
+        tlType = "run";
+      };
+    } ''
+    ${unzip} $src -d ${name}
+    mkdir -p $out/tex/latex/${name}
+    cp $(${rg} --files|${rg} sty) $out/tex/latex/${name}
+    ''
+  )];
+in {
+  options.texlive.enable = true;
   config = {
     environment.systemPackages = with pkgs; [
       (texlive.combine {
@@ -31,22 +26,19 @@ rec {
         xltabular ltablex makecell enumitem relsize
         blindtext apacite pgfplots chngcntr;
 
-        tabto = options.texpkg {
-          inherit pkgs;
+        tabto.pkgs = texpkg {
           name = "tabto";
           url = "https://mirrors.ctan.org/macros/latex/contrib/tabto.zip";
           sha256 = "sha256-fQku5SSg+4ENhlfKh6U8txdhhbCtGwlSSUyKepNpoZ4=";
           version = "1.4";
         };
-        titlesec = options.texpkg {
-          inherit pkgs;
+        titlesec.pkgs = texpkg {
           name = "titlesec";
           url = "https://mirrors.ctan.org/macros/latex/contrib/titlesec.zip";
           sha256 = "sha256-HL3dfcsnmejRxuL+dNxDKggx8V8vRyR010BwxziTjTA=";
           version = "2.17 2025-01-04";
         };
-        chngpage = options.texpkg {
-          inherit pkgs;
+        chngpage.pkgs = texpkg {
           name = "chngpage";
           url = "https://mirrors.ctan.org/install/macros/latex/contrib/changepage.tds.zip";
           sha256 = "sha256-tKImJD+97PVaz4HvJGkJbAzvIPwL3SVZWpxYU0zkdkM=";
