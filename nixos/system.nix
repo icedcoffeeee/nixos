1620f62033d9e79e11749eb66fd1155c7806776b
icedcoffeeee <83535735+icedcoffeeee@users.noreply.github.com>
2025-07-25 22:44:52 +0800

    usb cam

--- nixos/system.nix
+++ nixos/system.nix
@@ -4,7 +4,7 @@
   imports = [ ];
 
   environment.systemPackages = with pkgs; [
-    wget tree curl vim git gh xclip zip unzip xdg-utils
+    wget tree curl vim git gh xclip zip unzip xdg-utils usbutils
     gnumake cmake ffmpeg cairo pango dconf2nix nodejs_24 bun
     gcc14 clang-tools pandoc perl rustup go gdb tree-sitter
     lazygit lazydocker ripgrep fastfetch popsicle libreoffice
