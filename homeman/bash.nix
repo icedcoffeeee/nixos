94fc4a18fffbfc4cd16b1d5149db1cf2625c2adb
icedcoffeeee <83535735+icedcoffeeee@users.noreply.github.com>
2025-09-28 01:30:04 +0800

    alias

--- homeman/bash.nix
+++ homeman/bash.nix
@@ -1,39 +1,44 @@
 { ... }:
-{
+let
+  aliases = {
+    ga = "git add";
+    gc = "git commit";
+    gs = "git status";
+    gd = "git diff --cached";
+    ld = "lazydocker";
+    lg = "lazygit";
+    ll = "ls -alF";
+    mk = "mkdir -p";
+    ps = "ps -A|rg";
+    rgf = "rg --files|rg ";
+
+    ns = "nix-shell";
+    nrs = "nixos-rebuild switch --flake ~/nixos";
+    ncg =
+      "nix-collect-garbage -d && sudo /run/current-system/bin/switch-to-configuration boot";
+
+    s = "sudo ";
+    v = "nvim";
+    vx = "cd ~/nixos/ && nvim";
+
+    ".." = "cd ..";
+    open = "xdg-open";
+    camset = "sudo modprobe v4l2loopback";
+    cam0 = ''
+      scrcpy --v4l2-sink=/dev/video0 \
+      --video-source=camera --no-audio \
+      --camera-id=0 -m500 --no-window
+    '';
+    cam1 = ''
+      scrcpy --v4l2-sink=/dev/video0 \
+      --video-source=camera --no-audio \
+      --camera-id=1 -m500 --no-window
+    '';
+  };
+in {
   programs.bash = {
     enable = true;
-    shellAliases = {
-      ga = "git add";
-      gc = "git commit";
-      gs = "git status";
-      gd = "git diff --cached";
-      ld = "lazydocker";
-      lg = "lazygit";
-      ll = "ls -alF";
-      mk = "mkdir -p";
-      ps = "ps -A|rg";
-
-      nrs = "nixos-rebuild switch --flake ~/nixos";
-      ncg = "nix-collect-garbage -d && sudo /run/current-system/bin/switch-to-configuration boot";
-
-      s  = "sudo ";
-      v  = "nvim";
-      vx = "cd ~/nixos/ && nvim";
-
-      ".."   = "cd ..";
-      open   = "xdg-open";
-      camset = "sudo modprobe v4l2loopback";
-      cam0    = ''
-        scrcpy --v4l2-sink=/dev/video0 \
-        --video-source=camera --no-audio \
-        --camera-id=0 -m500 --no-window
-        '';
-      cam1    = ''
-        scrcpy --v4l2-sink=/dev/video0 \
-        --video-source=camera --no-audio \
-        --camera-id=1 -m500 --no-window
-        '';
-    };
+    shellAliases = aliases;
     bashrcExtra = ''
       export PATH=$PATH:/home/icedtea/.local/bin
       export PATH=$PATH:/home/icedtea/.cargo/bin
@@ -44,44 +49,12 @@
         repo=$1; shift 1;
         git clone "https://github.com/$repo.git" $@;
       }
-      '';
+    '';
   };
 
   programs.zsh = {
     enable = true;
-    shellAliases = {
-      ga = "git add";
-      gc = "git commit";
-      gs = "git status";
-      gd = "git diff --cached";
-      ld = "lazydocker";
-      lg = "lazygit";
-      ll = "ls -alF";
-      mk = "mkdir -p";
-      ps = "ps -A|rg";
-
-      ns = "nix-shell";
-      nrs = "nixos-rebuild switch --flake ~/nixos";
-      ncg = "nix-collect-garbage -d && sudo /run/current-system/bin/switch-to-configuration boot";
-
-      s  = "sudo ";
-      v  = "nvim";
-      vx = "cd ~/nixos/ && nvim";
-
-      ".."   = "cd ..";
-      open   = "xdg-open";
-      camset = "sudo modprobe v4l2loopback";
-      cam0    = ''
-        scrcpy --v4l2-sink=/dev/video0 \
-        --video-source=camera --no-audio \
-        --camera-id=0 -m500 --no-window
-        '';
-      cam1    = ''
-        scrcpy --v4l2-sink=/dev/video0 \
-        --video-source=camera --no-audio \
-        --camera-id=1 -m500 --no-window
-        '';
-    };
+    shellAliases = aliases;
     envExtra = ''
       export PATH=$PATH:/home/icedtea/.local/bin
       export PATH=$PATH:/home/icedtea/.cargo/bin
@@ -92,6 +65,6 @@
         repo=$1; shift 1;
         git clone "https://github.com/$repo.git" $@;
       }
-      '';
+    '';
   };
 }
